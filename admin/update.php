af31e1aba00e88ea4f48ab4f55352c4b134aa929
Abyravka90 <asepcahya.nugraha75@gmail.com>
2020-06-14 08:25:14 +0700

    mengubah file update.php logikanya sudah dapat

--- admin/update.php
+++ admin/update.php
@@ -2,7 +2,8 @@
 include '../config/database.php';
 $sql_technic = "SELECT nilai_technic FROM `point` ORDER BY nilai_technic DESC LIMIT 3";
 $sql_athletic = "SELECT nilai_athletic FROM `point` ORDER BY nilai_athletic DESC LIMIT 3";
-
+$sql_atribut = "SELECT distinct id_atlet, nama_atlet, kontingen, pertandingan_ke, grup FROM `point` ";//ini adalah query untuk mengambil semua value dari tabel poin untuk selanjutnya datanya diinsert ke table tabel_record
+$sql_dihitung = "SELECT SUM(dinilai) as total FROM `point`";//menghitung jumlah Juri yang menilai pada setiap pertandingan
 if ($result = $mysqli->query($sql_technic)) {
     while ($obj = $result->fetch_object()) {
         $subtotal_nilai_technic[] = $obj->nilai_technic;
@@ -15,5 +16,22 @@ if ($hasil = $mysqli->query($sql_athletic)) {
     }
     $total_nilai_athletic = 0.3 * array_sum($subtotal_nilai_athletic);
 }
-
-$total_nilai_semua = round($total_nilai_athletic + $total_nilai_technic, 2);
+if ($atlet = $mysqli->query($sql_atribut)){
+    while($obj3 = $atlet->fetch_object()){
+        $id_atlet = $obj3->id_atlet;
+        $nama_atlet = $obj3->nama_atlet;
+        $kontingen = $obj3->kontingen;
+        $pertandingan_ke = $obj3->pertandingan_ke;
+        $grup = $obj3->grup;
+    }
+}
+if ($hitung_juri_menilai = $mysqli->query($sql_dihitung)){
+    while($obj4 = $hitung_juri_menilai->fetch_object()){
+        $total_juri_menilai = $obj4->total;
+    }
+}
+$total_nilai_semua = number_format($total_nilai_athletic + $total_nilai_technic, 2);
+if($total_juri_menilai==0){
+    $sql_record = "INSERT INTO tabel_record (id_atlet, nama_atlet, kontingen, grup, pertandingan_ke, nilai_keseluruhan) VALUES ('".$id_atlet."','".$nama_atlet."','".$kontingen."','".$grup."','".$pertandingan_ke."','".$total_nilai_semua."')";
+    echo $sql_record;
+}//kalau semua juri sudah menilai maka masukan datanya
